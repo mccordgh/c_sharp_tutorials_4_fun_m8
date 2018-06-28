961efff0788d5ddf371fb50262fe2ffff96fa971
Matthew McCord <mattm.eternal@gmail.com>
2018-06-27 22:48:10 -0500

    Classes

---
+++ Classes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Matty", 1000);
+
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(100, DateTime.Now, "friend paid me back");
+            Console.WriteLine(account.Balance);
+
+            // Test for a negative balance
+            try
+            {
+                account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
+            }
+            catch
+            {
+                Console.WriteLine("Withdrawal not allowed. Will overdraft.");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(account.GetAccountHistory());
+        }
+    }
+}
---
+++ Classes/Transaction.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Classes
+{
+    public class Transaction
+    {
+        public decimal Amount { get; }
+        public DateTime Date { get; }
+        public string Note { get; }
+
+        public Transaction(decimal amount, DateTime date, string note)
+        {
+            this.Amount = amount;
+            this.Date = date;
+            this.Note = note;
+        }
+    }
+}
